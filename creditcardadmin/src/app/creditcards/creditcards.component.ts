b5f9d183076788b837fa64e873845d5d4720aac2
AliikaPSP <aliika.puusepp@hotmail.com>
2024-05-27 22:14:51 +0300

    ngDestroy + Matsnackbar

--- creditcardadmin/src/app/creditcards/creditcards.component.ts
+++ creditcardadmin/src/app/creditcards/creditcards.component.ts
@@ -2,7 +2,7 @@ import { Component, ViewChild } from '@angular/core';
 import { CreditCard } from '../models/credit-card';
 import { MatTableDataSource } from '@angular/material/table';
 import { SelectionModel } from '@angular/cdk/collections';
-import { MatPaginator } from '@angular/material/paginator';
+import { MatPaginator} from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { CreditcardsService } from '../services/creditcards.service';
 
@@ -15,19 +15,24 @@ export class CreditcardsComponent {
 
   creditcards: CreditCard[] = [];
 
-  constructor(private creditcardsService: CreditcardsService) {
-    this.creditcardsService.getCreditCards().subscribe((data:CreditCard[]) => {
+  creditCardMaximumAmount: number = 0;
+  creditCardMaximumInterest: number = 0;
+
+  constructor(private creditCardsService: CreditcardsService) {
+    this.creditCardsService.getCreditCards().subscribe((data:CreditCard[]) => {
       this.creditcards = data;
 
       this.dataSource = new MatTableDataSource(this.creditcards);
       this.dataSource.paginator = this.paginator;
       this.dataSource.sort = this.sort;
 
+      this.calculateMetrics();
+
     })
   }
 
   dataSource = new MatTableDataSource(this.creditcards);
-  
+
   displayColumns = ["select", "id", "name", "description", "bankName", "maxCredit", "interestRate", "active", "recommendedScore", "actions"];
 
   selection = new SelectionModel(true, []);
@@ -35,8 +40,13 @@ export class CreditcardsComponent {
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
-  selectHandler(row: CreditCard) {
+  selectHandler(row: CreditCard){
     this.selection.toggle(row as never);
   }
 
+  calculateMetrics(){
+    this.creditCardMaximumAmount = this.creditcards.filter(card => card.maxCredit > 3000).length;
+    this.creditCardMaximumInterest = this.creditcards.filter(card => card.interestRate > 7).length;
+    
+  }
 }
